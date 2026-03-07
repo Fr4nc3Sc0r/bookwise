b0e50467b242e5bb1e8130b700c07bb74e6a6a6c
Fara <lfrancesco157@gmail.com>
2026-03-07 09:04:02 +0100

    ho impostato le route e utilizzato per la prima volta l'interfaccia di fastapi, va bene

--- backend/db/database.py
+++ backend/db/database.py
@@ -6,7 +6,7 @@ DATABASE_URL = "sqlite:///./bookwise.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base
+Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
